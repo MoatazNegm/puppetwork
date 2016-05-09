01bc27e74644c83c3efbd8332a0879ba84bcb413
Mezo <sales@quickstor.net>
2016-05-09 19:06:03 -0400

    progress

--- files/keystonech2.sh
+++ files/keystonech2.sh
@@ -1,13 +1,42 @@
 #!/bin/sh
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/root
 pass=`echo $@ | awk '{print $1}'`;
+pass2=`echo $@ | awk '{print $2}'`;
+cont=`echo $@ | awk '{print $3}'`;
 source keystonesource
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/root
-users=`openstack user list | wc -l`;
-echo users = $users
-if [ $users -lt 2 ]; then
- source keystonesource
+roles=`openstack role list` 
+echo $roles | grep admin &>/dev/null 
+if [ $? -ne 0 ]; then
  openstack domain create --description "Default Domain" default 
+ openstack project create --domain default --description "Service Project" service 
  openstack project create --domain default --description "Admin Project" admin 
  openstack user create --domain default  --password $pass admin 
+ openstack role create admin
+ openstack role add --project admin --user admin admin
 fi
+
+echo $roles | grep user  &>/dev/null 
+if [ $? -ne 0 ]; then
+ openstack project create --domain default --description "Demo Project" demo 
+ openstack user create --domain default  --password $pass2 demo 
+ openstack role create user 
+ openstack role add --project demo --user demo user 
+fi
+echo export OS_PROJECT_DOMAIN_NAME=default > admin-openrc
+echo export OS_USER_DOMAIN_NAME=default >> admin-openrc
+echo export OS_PROJECT_NAME=admin >> admin-openrc
+echo export OS_USERNAME=admin >> admin-openrc
+echo export OS_PASSWORD=$pass >> admin-openrc
+echo export OS_AUTH_URL=http://$cont:35357/v3 >> admin-openrc
+echo export OS_IDENTITY_API_VERSION=3 >> admin-openrc
+echo export OS_IMAGE_API_VERSION=2 >> admin-openrc
+
+echo export OS_PROJECT_DOMAIN_NAME=default > demo-openrc
+echo export OS_USER_DOMAIN_NAME=default >> demo-openrc
+echo export OS_PROJECT_NAME=demo >> demo-openrc
+echo export OS_USERNAME=demo >> demo-openrc
+echo export OS_PASSWORD=$pass2 >> demo-openrc
+echo export OS_AUTH_URL=http://$cont:5000/v3 >> demo-openrc
+echo export OS_IDENTITY_API_VERSION=3 >> demo-openrc
+echo export OS_IMAGE_API_VERSION=2 >> demo-openrc
--- manifests/classes/identity.pp
+++ manifests/classes/identity.pp
@@ -15,7 +15,10 @@ $rabbit_user = 'openstack'
 $rabbit_pass = 'tmatem'
 */
 $keystonedb_pass = 'tmatem'
-$defualtDom_pass = 'tmatem'
+$defualtAdmin_pass = 'tmatem'
+$defualtDemo_pass = 'tmatem'
+$glancedb_pass = 'tmatem'
+$glanceuser_pass = 'tmatem'
 class identity {
         file { '/root/identitysql.sh':
         mode => 755,
@@ -27,7 +30,7 @@ class identity {
 	command => "/bin/sh identitysql.sh $mysql_pass CC $keystonedb_pass",
 	subscribe => File['/root/identitysql.sh'],
 	}
-	package { [ 'openstack-keystone', 'httpd', 'mod_wsgi' ]:
+	package { [ 'openstack-keystone', 'httpd', 'mod_wsgi', 'openstack-glance' ]:
 	ensure => 'installed',
 	}	
         file { '/etc/httpd/conf.d/wsgi-keystone.conf':
@@ -73,8 +76,44 @@ class identity {
 	}
 	exec { 'keystonech2':
 	cwd => '/root',
-	command => "/bin/sh keystonech2.sh $defualtDom_pass",
+	command => "/bin/sh keystonech2.sh $defualtAdmin_pass $defualtDemo_pass CC",
 	logoutput => true,
 	subscribe => [ File['/root/keystonech2.sh'], Exec['identitch'] ],
+	}
+        file { '/root/glance-registry.conf':
+        mode => 755,
+        source => 'puppet:///extra_files/glance-registry.conf',
+	ensure => 'file',
+	subscribe => Exec['identitch'],
+	}
+        file { '/root/glance-api.conf':
+        mode => 755,
+        source => 'puppet:///extra_files/glance-api.conf',
+	ensure => 'file',
+	subscribe => Exec['identitch'],
+	}
+        file { '/root/glancech.sh':
+        mode => 755,
+        source => 'puppet:///extra_files/glancech.sh',
+	ensure => 'file',
+	subscribe => Exec['identitch'],
+	}
+	exec { 'glancech':
+	cwd => '/root',
+	command => "/bin/sh glancech.sh $mysql_pass CC $glancedb_pass $glanceuser_pass",
+	logoutput => true,
+	subscribe => [ File['/root/glancech.sh'], Exec['keystonech2'] ],
+	}
+        file { '/root/glancech2.sh':
+        mode => 755,
+        source => 'puppet:///extra_files/glancech2.sh',
+	ensure => 'file',
+	subscribe => Exec['identitch'],
+	}
+	exec { 'glancech2':
+	cwd => '/root',
+	command => "/bin/sh glancech2.sh $glancedb_pass CC $CC $glanceuser_pass $share || :",
+	logoutput => true,
+	subscribe => [ File['/root/glancech2.sh'], Exec['glancech'], File['/root/glance-api.conf'], File['/root/glance-registry.conf'], Package['openstack-glance'] ],
 	}
 }
