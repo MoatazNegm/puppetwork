71dd8b65409870de01cf7a0d23f6c9cede91ee1f
mezo <sales@quickstor.net>
2018-03-30 18:52:58 +0200

    fixing

--- modules/zfs/files/preparemsg.sh
+++ modules/zfs/files/preparemsg.sh
@@ -4,6 +4,7 @@ export PATH=/root/elixir/bin:/bin:/usr/bin:/sbin:/usr/sbin:/root
 #make clean test
 #cp /root/elixir/bin/*  /usr/bin
 cd ~/elixir
+python3.6 -m pip install python-nmap-0.6.1.tar.gz
 yum install -y rabbitmq-server-3.6.1-1.noarch.rpm
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq/
 systemctl start rabbitmq-server
