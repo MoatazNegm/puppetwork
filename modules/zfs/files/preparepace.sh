dbf07ccd2ba2327aed14e0e4b4f032de93865ba8
mezo <sales@quickstor.net>
2018-04-06 16:30:45 +0200

    fixing

--- modules/zfs/files/preparepace.sh
+++ modules/zfs/files/preparepace.sh
@@ -12,8 +12,8 @@ if [ $? -ne 0 ]; then
  git remote add origin http://localrepo/HC.git
  git remote add origin2 http://github.com/MoatazNegm/HC.git
  git fetch origin
- git checkout -b QS1.61
- git pull origin QS1.61
+ git checkout -b QS1.85
+ git pull origin QS1.85
  echo $hostip $hostname > /pacedata/iscsitargets
  echo $hostip $hostname > /pace/iscsitargets
 fi
