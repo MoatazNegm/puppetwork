f0051200c1bf92392356c3b96bfb773401547203
mezo <sales@quickstor.net>
2020-06-20 21:19:51 +0200

    fixing

--- modules/zfs/files/preparegraphite.sh
+++ modules/zfs/files/preparegraphite.sh
@@ -13,3 +13,4 @@ then
  docker run -d --name=nodeprm --restart=always -p 9100:9100 -v "/proc:/host/proc" -v "/sys:/host/sys" -v "/:/rootfs" --net="host" 10.11.11.124:5000/nodeprm --path.procfs /host/proc --path.sysfs /host/sys --collector.filesystem.ignored-mount-points "^(sys|dev|proc|host|etc)($|/)" --collector.textfile.directory "/rootfs/TopStordata"
  docker run -d --name=grafana --restart=always -p 3000:3000 -v /TopStordata/grafana.files/hosts:/etc/hosts -v "/TopStordata/grafana:/etc/grafana" 10.11.11.124:5000/grafana
 fi
+docker pull 10.11.11.124:5000/smb
