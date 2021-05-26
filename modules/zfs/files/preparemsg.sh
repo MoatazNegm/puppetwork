18f66849aaf53fb9342a36931309ba95bbcb1d0f
mezo <sales@quickstor.net>
2021-05-27 01:23:47 +0200

    fixin

--- modules/zfs/files/preparemsg.sh
+++ modules/zfs/files/preparemsg.sh
@@ -5,7 +5,7 @@ export PATH=/root/elixir/bin:/bin:/usr/bin:/sbin:/usr/sbin:/root
 #cp /root/elixir/bin/*  /usr/bin
 cd ~/elixir
 python3.6 -m pip install python-nmap-0.6.1.tar.gz
-python3.6 -m pip install Flask-2.0.0.tar.gz
+python3.6 -m pip install *.whl
 python3.6 -m pip install prometheus_client-0.6.0.tar.gz
 yum install -y rabbitmq-server
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq/
