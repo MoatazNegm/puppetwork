04f004889304c42bf3e7819e3fd606e088b66d3f
dlksj.ewjf <dklljfd@sldjf.com>
2021-08-14 18:22:33 +0200

    fxiing

--- modules/zfs/files/preparetop.sh
+++ modules/zfs/files/preparetop.sh
@@ -51,10 +51,12 @@ if [ $? -ne 0 ]; then
  git remote set-url --push origin2 https://github.com/MoatazNegm/TopStorweb.git
  git checkout -b QS2.84
  git pull origin QS2.84
- rm -rf dashboarddev2/dist/img/*
- cp img/* dashboarddev2/dist/img/
+ rm -rf dashboarddev2/img/*
+ cp img/* dashboarddev2/img/
  rm -rf dashboarddev2/plugins/fontawesome-free/*
  cp -r plugins/fontawesome-free/* dashboarddev2/plugins/fontawesome-free/
+ rm -rf dashboarddev2/plugins/gfont/*
+ cp -r plugins/gfont/* dashboarddev2/plugins/gfont/
  
  mkdir Data
  chown apache Data -R
