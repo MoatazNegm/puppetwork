3a7da15dd991cff3fdfc66cb2668f4a9862a8f94
dlksj.ewjf <dklljfd@sldjf.com>
2021-08-21 12:33:28 +0200

    fixing

--- modules/zfs/files/preparetop.sh
+++ modules/zfs/files/preparetop.sh
@@ -52,7 +52,9 @@ if [ $? -ne 0 ]; then
  git checkout -b QS2.84
  git pull origin QS2.84
  rm -rf dashboarddev2/img/*
+ rm -rf dashboarddev2/dist/img/*
  cp img/* dashboarddev2/img/
+ cp img/* dashboarddev2/dist/img/
  rm -rf dashboarddev2/plugins/fontawesome-free/*
  cp -r plugins/fontawesome-free/* dashboarddev2/plugins/fontawesome-free/
  rm -rf dashboarddev2/plugins/gfont/*
