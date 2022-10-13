7a4a89f25ae319b99c94e58989aef43f8e32c11d
mezo <sales@quickstor.net>
2022-10-13 21:53:14 +0200

    fixing

--- modules/zfs/files/preparegraphite.sh
+++ modules/zfs/files/preparegraphite.sh
@@ -6,7 +6,8 @@ myhost=`hostname`
 cp /root/daemon.json /etc/docker/
 systemctl enable docker
 systemctl start docker
-if [ $? -eq 0 ];
+#if [ $? -eq 0 ];
+if [ 1 -eq 0 ];
 then
 # docker run -d --name=graphite --restart=always -p 8080:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126:8126 10.11.11.124:5000/graphite
  docker run -d --name=prom --restart=always -p 9090:9090 -v /TopStordata/prometheus.files/hosts:/etc/hosts -v /TopStordata/prometheus.files/prometheus.yml:/etc/prometheus/prometheus.yml 10.11.11.124:5000/prom  
